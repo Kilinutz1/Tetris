b31d2d1d27398acd226fdc6b97561d6298b3893f
Feli <fungerhofer@gmail.com>
2021-06-25 08:38:33 +0200

    RZ vorbereitet

--- HERO.java
+++ HERO.java
@@ -1,24 +1,22 @@
-
-
 public class HERO extends Bausteine
 {
-public HERO(){
-    
-}
-@Override
-public void drehen(){
-    
-}
-@Override
-public void herunterfallen(){
-    
-}
-@Override
-public void seitlichbewegen(){
-    
-}
-@Override
-public void aufkommen(){
-    
-}
+    public HERO(){
+        
+    }
+    @Override
+    public void drehen(){
+        
+    }
+    @Override
+    public void herunterfallen(){
+        
+    }
+    @Override
+    public void seitlichbewegen(){
+        
+    }
+    @Override
+    public void aufkommen(){
+        
+    }
 }
--- RhodeIslandZ.java
+++ RhodeIslandZ.java
@@ -5,29 +5,25 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class RhodeIslandZ
+public class RhodeIslandZ extends Bausteine
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class RhodeIslandZ
-     */
-    public RhodeIslandZ()
-    {
-        // initialise instance variables
-        x = 0;
+    public RhodeIslandZ(){
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    @Override
+    public void drehen(){
+        
+    }
+    @Override
+    public void herunterfallen(){
+        
+    }
+    @Override
+    public void seitlichbewegen(){
+        
+    }
+    @Override
+    public void aufkommen(){
+        
     }
 }
