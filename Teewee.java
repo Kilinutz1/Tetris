cfa08d3a9f2eb580b4439394f11c96ea96d751b2
KiliNutZ1 <Kili.schreiner@gmail.com>
2021-06-25 08:36:13 +0200

    Klassen

--- Teewee.java
+++ Teewee.java
@@ -1,33 +1,29 @@
 
-/**
- * Write a description of class Teewee here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Teewee
+public class Teewee extends Bausteine
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Teewee
-     */
     public Teewee()
     {
-        // initialise instance variables
-        x = 0;
+
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    @Override
+    public void drehen(){
+
+    }
+
+    @Override
+    public void herunterfallen(){
+
+    }
+
+    @Override
+    public void seitlichbewegen(){
+
+    }
+
+    @Override
+    public void aufkommen(){
+
+    
     }
 }
