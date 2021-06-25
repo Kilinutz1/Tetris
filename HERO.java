340db71b12f6d3672b976f42654e1da4d05ddd8a
KiliNutZ1 <Kili.schreiner@gmail.com>
2021-06-25 08:49:09 +0200

    HEROform

--- HERO.java
+++ HERO.java
@@ -1,7 +1,14 @@
 public class HERO extends Bausteine
 {
-    public HERO(){
-    x1= new QUADER;    
+    QUADER x1;
+    QUADER x2;
+    QUADER x3;
+    QUADER x4;
+    public HERO(){  
+     x1 = new QUADER(1,1,"blau"); 
+     x2 = new QUADER(2,1,"blau");
+     x3 = new QUADER(3,1,"blau");
+     x4 = new QUADER(4,1,"blau");
     }
     @Override
     public void drehen(){
