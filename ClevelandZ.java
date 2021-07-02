898ac4e8e8985b09c10dc7371968d3c44ce39b87
Kili <Kili.schreiner@gmail.com>
2021-07-02 09:28:07 +0200

    wdw

--- ClevelandZ.java
+++ ClevelandZ.java
@@ -24,7 +24,68 @@ public class ClevelandZ extends Bausteine
     }
     @Override
     public void drehen(){
-        
+    if (Ausrichtung == 'n'){
+     eins.PositionSetzen(einsx+1,einsy-1);
+     zwei.PositionSetzen(zweix ,zweiy);
+     drei.PositionSetzen(dreix-1,dreiy-1);
+     vier.PositionSetzen(vierx-2,viery);
+     einsx = einsx+1;
+     einsy = einsy-1;
+     zweix =zweix;
+     zweiy =zweiy;
+     dreix =dreix-1;
+     dreiy =dreiy-1;
+     vierx =vierx-2;
+     viery = viery;
+     Ausrichtung ='o';
+    }
+    else if (Ausrichtung == 'o'){
+    eins.PositionSetzen(einsx+1,einsy+1);
+     zwei.PositionSetzen(zweix ,zweiy);
+     drei.PositionSetzen(dreix+1,dreiy-1);
+     vier.PositionSetzen(vierx,viery-2);
+     einsx = einsx+1;
+     einsy = einsy+1;
+     zweix =zweix ;
+     zweiy =zweiy;
+     dreix =dreix+1;
+     dreiy =dreiy-1;
+     vierx =vierx;
+     viery = viery-2;
+    Ausrichtung ='s';
+    }
+    else if (Ausrichtung == 's'){
+     eins.PositionSetzen(einsx-1,einsy+1);
+     zwei.PositionSetzen(zweix,zweiy);
+     drei.PositionSetzen(dreix+1,dreiy+1);
+     vier.PositionSetzen(vierx+2,viery);
+     einsx = einsx-1;
+     einsy = einsy+1;
+     zweix =zweix ;
+     zweiy =zweiy;
+     dreix =dreix+1;
+     dreiy =dreiy+1;
+     vierx =vierx+2;
+     viery = viery;
+    Ausrichtung ='w';
+    
+    }
+    else if (Ausrichtung == 'w'){
+    eins.PositionSetzen(einsx-1,einsy-1);
+     zwei.PositionSetzen(zweix ,zweiy);
+     drei.PositionSetzen(dreix-1,dreiy+1);
+     vier.PositionSetzen(vierx,viery+2);
+     einsx = einsx-1;
+     einsy = einsy-1;
+     zweix =zweix ;
+     zweiy =zweiy;
+     dreix =dreix-1;
+     dreiy =dreiy+1;
+     vierx =vierx;
+     viery = viery+2;
+    
+    Ausrichtung ='n';
+}
     }
     
     @Override
